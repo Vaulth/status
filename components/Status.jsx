248a4c6482b536b3764cc674186096e704d567e2
mystère <contact@mystere.dev>
2023-06-29 14:54:28 +0900

    Set indvidual usestates for each service.

--- components/Status.jsx
+++ components/Status.jsx
@@ -6,48 +6,110 @@ import React, { useState, useEffect } from "react"
 
 const axios = require("axios");
 
+const STATUS = {
+    Fetching: 0,
+    Offline: 1,
+    Running: 2,
+};
+
 const checkWebsite = async (url) => {
-    let websiteStatus = false;
+    let websiteStatus = STATUS.Offline;
 
     await axios.get(url)
         .then((response) => {
             console.log("SUCCESS: checkWebsite ({" + url + "}): " + response.status);
-            websiteStatus = true;
+            websiteStatus = STATUS.Running;
         })
         .catch((error) => {
             console.log("ERROR: checkWebsite ({" + url + "}): " + error.code);
-            websiteStatus = false;
+            websiteStatus = STATUS.Offline;
         })
 
     return websiteStatus
 }
 
-export function useStatus() {
-    const [status, setStatus] = useState({
-        backend: false,
-        dashboard: false,
-        landingPage: false,
-        support: false,
-        blog: false,
-        portal: false
-    });
+export function useBackendStatus() {
+    const [backendStatus, setBackendStatus] = useState(STATUS.Fetching);
 
     useEffect(() => {
         const checkStatus = async () => {
-            setStatus({
-                backend: await checkWebsite("https://backend.vaulth.app/"),
-                dashboard: await checkWebsite("https://dashboard.vaulth.app/"),
-                landingPage: await checkWebsite("https://vaulth.app/"),
-                support: await checkWebsite("https://support.vaulth.app/"),
-                blog: await checkWebsite("https://blog.vaulth.app/"),
-                portal: await checkWebsite("https://portal.vaulth.app/"),
-            });
+            setBackendStatus(await checkWebsite("https://backend.vaulth.app/"));
         }
 
         checkStatus();
     }, []);
 
-    return status;
+    return backendStatus;
+}
+
+export function useDashboardStatus() {
+    const [dashboardStatus, setDashboardStatus] = useState(STATUS.Fetching);
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            setDashboardStatus(await checkWebsite("https://dashboard.vaulth.app/"));
+        }
+
+        checkStatus();
+    }, []);
+
+    return dashboardStatus;
+}
+
+export function useLandingpageStatus() {
+    const [landingpageStatus, setLandingpageStatus] = useState(STATUS.Fetching);
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            setLandingpageStatus(await checkWebsite("https://vaulth.app/"));
+        }
+
+        checkStatus();
+    }, []);
+
+    return landingpageStatus;
+}
+
+export function useSupportStatus() {
+    const [supportStatus, setSupportStatus] = useState(STATUS.Fetching);
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            setSupportStatus(await checkWebsite("https://support.vaulth.app/"));
+        }
+
+        checkStatus();
+    }, []);
+
+    return supportStatus;
+}
+
+export function useBlogStatus() {
+    const [blogStatus, setBlogStatus] = useState(STATUS.Fetching);
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            setBlogStatus(await checkWebsite("https://blog.vaulth.app/"));
+        }
+
+        checkStatus();
+    }, []);
+
+    return blogStatus;
+}
+
+export function usePortalStatus() {
+    const [portalStatus, setPortalStatus] = useState(STATUS.Fetching);
+
+    useEffect(() => {
+        const checkStatus = async () => {
+            setPortalStatus(await checkWebsite("https://portal.vaulth.app/"));
+        }
+
+        checkStatus();
+    }, []);
+
+    return portalStatus;
 }
 
 export function useStatistics() {
@@ -83,7 +145,12 @@ export function useStatistics() {
 }
 
 export const Status = () => {
-    const status = useStatus();
+    const backendStatus = useBackendStatus();
+    const dashboardStatus = useDashboardStatus();
+    const landingpageStatus = useLandingpageStatus();
+    const supportStatus = useSupportStatus();
+    const blogStatus = useBlogStatus();
+    const portalStatus = usePortalStatus();
     const statistics = useStatistics();
 
     return (
@@ -91,12 +158,12 @@ export const Status = () => {
             <div className={styles.title}>Official <b>Vaulth</b> services status</div>
             <div className={styles.statusDiv}>
                 <div className={styles.desc}>Services</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.backend ? colorStyles.successBg : colorStyles.errorBg)} />Backend is {status.backend ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.dashboard ? colorStyles.successBg : colorStyles.errorBg)} />Dashboard is {status.dashboard ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.landingPage ? colorStyles.successBg : colorStyles.errorBg)} />Landing-Page is {status.landingPage ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.support ? colorStyles.successBg : colorStyles.errorBg)} />Support is {status.support ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.blog ? colorStyles.successBg : colorStyles.errorBg)} />Blog is {status.blog ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
-                <div className={styles.oneStat}><div className={styles.dot + " " + (status.portal ? colorStyles.successBg : colorStyles.errorBg)} />Portal is {status.portal ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (backendStatus ? colorStyles.successBg : colorStyles.errorBg)} />Backend is {backendStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (dashboardStatus ? colorStyles.successBg : colorStyles.errorBg)} />Dashboard is {dashboardStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (landingpageStatus ? colorStyles.successBg : colorStyles.errorBg)} />Landing-Page is {landingpageStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (supportStatus ? colorStyles.successBg : colorStyles.errorBg)} />Support is {supportStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (blogStatus ? colorStyles.successBg : colorStyles.errorBg)} />Blog is {blogStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
+                <div className={styles.oneStat}><div className={styles.dot + " " + (portalStatus ? colorStyles.successBg : colorStyles.errorBg)} />Portal is {portalStatus ? <span className={colorStyles.success}>up and running</span> : <span className={colorStyles.error}>offline</span>}</div>
             </div>
             <div className={styles.statusDiv}>
                 <div className={styles.desc}>Statistics</div>
